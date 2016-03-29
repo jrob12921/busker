1104670736d5760611a7411004822f21ced75448
Jordan Roberts <jrob12921@gmail.com>
2016-03-29 18:42:04 -0400

    The map works!

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,2 +1,15 @@
 class StationsController < ApplicationController
+  def index
+    @stations = Station.all
+    @hash = Gmaps4rails.build_markers(@stations) do |station, marker|
+      marker.lat station.lat
+      marker.lng station.long
+      marker.infowindow "<a href= 'stations/#{station.id}'>#{station.name}: #{station.line}</a>"
+    end    
+  end
+
+  def show
+    @station = Station.find(params[:id])
+  end
+
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,4 @@
 class Station < ActiveRecord::Base
   has_many :reviews
+
 end
