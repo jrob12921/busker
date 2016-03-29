2d349c77ed25122d22f6b6c9425938b448170b54
Jordan Roberts <jrob12921@gmail.com>
2016-03-29 16:26:14 -0400

    Added paperclip avatars to Profile and Review models.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,11 @@ class Review < ActiveRecord::Base
   belongs_to :station
 
   has_many :comments, dependent: :destroy
+
+  has_attached_file :avatar, 
+    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :default_url => "/images/:style/missing.png" 
+
+  validates_attachment_content_type :avatar, 
+    :content_type => /\Aimage\/.*\Z/
 end
---
+++ db/schema.rb
@@ -0,0 +1,93 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160329200044) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "review_id"
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["review_id"], name: "index_comments_on_review_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "username"
+    t.string   "fname"
+    t.string   "lname"
+    t.string   "borough"
+    t.string   "neighborhood"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "station_id"
+    t.string   "title"
+    t.text     "content"
+    t.boolean  "rating"
+    t.date     "date_seen"
+    t.string   "artist_name"
+    t.text     "instruments"
+    t.text     "add_info"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
+
+  add_index "reviews", ["station_id"], name: "index_reviews_on_station_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
+
+  create_table "stations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "line"
+    t.float    "lat"
+    t.float    "long"
+    t.boolean  "gmaps"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+end
