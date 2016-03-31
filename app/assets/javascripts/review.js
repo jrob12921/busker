6ccf79a69bc1ce83edc5fcd8bce2ce3c5848fb84
Jordan Roberts <jrob12921@gmail.com>
2016-03-31 18:36:42 -0400

    Replaced radio buttons with thumbs up/down.

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -4,6 +4,32 @@ $(document).ready(function(){
     window.document.location = $(this).data("href")
   })
 
-  
+// NEED TO ADD RADIO-CHECKED!!!
+
+  $("#down").click(function(){
+    if($("#down").attr("class","fa fa-thumbs-o-down")){
+
+      $("#up").attr("class","fa fa-thumbs-o-up")
+      $("#down").attr("class","fa fa-thumbs-down" )
+
+    }else{
+
+      $("#up").attr("class","fa fa-thumbs-up")
+      $("#down").attr("class","fa fa-thumbs-o-down" )
+    }
+  })
+
+  $("#up").click(function(){
+    if($("#up").attr("class","fa fa-thumbs-o-up")){
+
+      $("#up").attr("class","fa fa-thumbs-up")
+      $("#down").attr("class","fa fa-thumbs-o-down" )
+
+    }else{
+
+      $("#up").attr("class","fa fa-thumbs-o-up")
+      $("#down").attr("class","fa fa-thumbs-down" )
+    }
+  })
 
 })
