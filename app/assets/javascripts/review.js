31f72fd9933f88f96c3b64c2a759691887bb809f
Jordan Roberts <jrob12921@gmail.com>
2016-04-01 11:09:23 -0400

    Added in jquery for radio button in review new view.

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -7,28 +7,34 @@ $(document).ready(function(){
 // NEED TO ADD RADIO-CHECKED!!!
 
   $("#down").click(function(){
-    if($("#down").attr("class","fa fa-thumbs-o-down")){
 
-      $("#up").attr("class","fa fa-thumbs-o-up")
-      $("#down").attr("class","fa fa-thumbs-down" )
+    if(!$("#review_rating_down")[0].checked){
 
-    }else{
+      $("#review_rating_down")[0].checked = true
+      $("#down").attr("class","fa fa-5x fa-thumbs-down")
 
-      $("#up").attr("class","fa fa-thumbs-up")
-      $("#down").attr("class","fa fa-thumbs-o-down" )
+      $("#review_rating_up")[0].checked = false
+      $("#up").attr("class","fa fa-5x fa-thumbs-o-up")
+
+      // console.log($  ("#review_rating_up")[0])
+      // console.log($("#review_rating_down")[0])
+
     }
   })
 
   $("#up").click(function(){
-    if($("#up").attr("class","fa fa-thumbs-o-up")){
 
-      $("#up").attr("class","fa fa-thumbs-up")
-      $("#down").attr("class","fa fa-thumbs-o-down" )
+    if(!$("#review_rating_up")[0].checked){
 
-    }else{
+      $("#review_rating_up")[0].checked = true
+      $("#up").attr("class","fa fa-5x fa-thumbs-up")
 
-      $("#up").attr("class","fa fa-thumbs-o-up")
-      $("#down").attr("class","fa fa-thumbs-down" )
+      $("#review_rating_down")[0].checked = false
+      $("#down").attr("class","fa fa-5x fa-thumbs-o-down")
+
+      // console.log($("#review_rating_up")[0])
+      // console.log($("#review_rating_down")[0])
+
     }
   })
 
