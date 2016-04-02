197661728e650435b7336d4db4c32f1403cc3414
Jordan Roberts <jrob12921@gmail.com>
2016-04-02 17:41:57 -0400

    Added info_window icons.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,7 +4,16 @@ class StationsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@stations) do |station, marker|
       marker.lat station.lat
       marker.lng station.long
-      marker.infowindow "<a href= 'stations/#{station.id}'>#{station.name}: #{station.line}</a>"
+      lines_as_src = ""
+      station.line.split("-").each do |ln|
+        lines_as_src += "<img class='info_line_icon' src=\/assets\/line_icons\/#{ln.downcase}.svg\/> "
+      end
+      marker.infowindow "<a class='info_station' href='stations/#{station.id}'>#{station.name} #{lines_as_src}</a>"
+      marker.picture({
+        "picture" => "/images/train_icon.png",
+        "width" => 10,
+        "height" => 10
+      })
     end    
   end
 
