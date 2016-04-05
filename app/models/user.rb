405c9124c0eccabb459a46357a85a886784dab34
Jordan Roberts <jrob12921@gmail.com>
2016-04-05 14:06:34 -0400

    moved callback before relationship.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  has_one :profile, dependent: :destroy
-  has_many :reviews, dependent: :destroy
-  has_many :comments, dependent: :destroy
 
 
 
   after_create do
     Profile.create(user_id: self.id)
   end
+
+    devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :profile, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
