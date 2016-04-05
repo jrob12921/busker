503d5293767cfa0e9496d7364491eb13f49d2c7f
Jordan Roberts <jrob12921@gmail.com>
2016-04-05 15:01:48 -0400

    Fixed problem by putting code in devise new user view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-
-
-  after_create do
-    Profile.create(user_id: self.id)
-  end
-
-    devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :profile, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :comments, dependent: :destroy
+  
+
+
+  after_create do
+    Profile.create(:user => self)
+  end
+
+  accepts_nested_attributes_for :profile
+
 end
